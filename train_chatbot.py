4ace72eeca2dcdf93a4e3625c6feed34713486c9
unknown <kshashank231@gmail.com>
2020-06-02 11:14:29 +0530

    Custom training

--- train_chatbot.py
+++ train_chatbot.py
@@ -13,20 +13,11 @@ from keras.models import Sequential
 from keras.layers import Dense, Activation, Dropout
 from keras.optimizers import SGD
 import random
-
-
 import re, string, unicodedata
 
 
 
-
-
-words=[]
-classes = []
-documents = []
-#ignore_words = ['?', '!']
-data_file = open('intents.json').read()
-intents = json.loads(data_file)
+# Defining Functions
 
 def process_text(text):
   token_words = nltk.word_tokenize(text)
@@ -82,14 +73,20 @@ def lemmatize_verbs(words):
   return lemmas
 
 def normalize(words):
+    words = remove_non_ascii(words)
+    words = to_lowercase(words)
+    words = remove_punctuation(words)
+    words = remove_stopwords(words)
+    words = lemmatize_verbs(words)
+    
+    return words
 
-  words = remove_non_ascii(words)
-  words = to_lowercase(words)
-  words = remove_punctuation(words)
-  words = remove_stopwords(words)
-  
-  words = lemmatize_verbs(words)
-  return words
+# Extracting Data
+words=[]
+classes = []
+documents = []
+data_file = open('intents.json').read()
+intents = json.loads(data_file)
 
 for intent in intents['intents']:
     for pattern in intent['patterns']:
@@ -98,99 +95,53 @@ for intent in intents['intents']:
         w = nltk.word_tokenize(pattern)
         words.extend(w)
         #add documents in the corpus
-        documents.append((w, intent['tag']))
+        documents.append((pattern, intent['tag']))
 
         # add to our classes list
         if intent['tag'] not in classes:
             classes.append(intent['tag'])
-print(documents[0])
-print(documents[1])
-print(documents[2])
-# lemmaztize and lower each word and remove duplicates
-#words = normalize(words)
-words = sorted(list(set(words)))
-# sort classes
-classes = sorted(list(set(classes)))
-# documents = combination between patterns and intents
-print (len(documents), "documents")
-# classes = intents
-print (len(classes), "classes", classes)
-# words = all words, vocabulary
-print (len(words), "unique lemmatized words", words)
 
+# Creating a DataFrame and shuffling
+documents_df = pd.DataFrame(documents,columns=['pattern','classes'])
+documents_df = documents_df.sample(frac=1,random_state=42).reset_index().drop('index',axis=1)
+
+
+y = pd.get_dummies(documents_df.classes)
+unique_classes = y.columns
+unique_classes
+
+# Creating Vocabulary List
+
+words = normalize(words)
+words = sorted(list(set(words)))
+
+
+# Saving words and unique_classes Objects
 
 pickle.dump(words,open('words.pkl','wb'))
-pickle.dump(classes,open('classes.pkl','wb'))
+pickle.dump(unique_classes,open('classes.pkl','wb'))
 
-# create our training data
-training = []
-# create an empty array for our output
-output_empty = [0] * len(classes)
+#Creating training set
 
+count_vect = CountVectorizer(analyzer = normalize,vocabulary=words)
+X_train = count_vect.fit_transform(documents_df.pattern.apply(process_text)).toarray()
+y_train = y.to_numpy()
 
-documents_df = pd.DataFrame(documents,columns=['pattern','classes'])
-y_train = pd.get_dummies(documents_df.classes)
-count_vect = CountVectorizer(analyzer=normalize)
-words_count = count_vect.fit(words)
-X_train =  count_vect.transform(documents_df.pattern)
-print(X_train.shape)
-# print(type(bag))
-# print(bag.shape)
-# print('-----------------')
-# # training set, bag of words for each sentence
-# for doc in documents:
-#     # initialize our bag of words
-#     #bag = []
-#     # list of tokenized words for the pattern
-#    #pattern_words = doc[0]
-#     # lemmatize each word - create base word, in attempt to represent related words
-#     #pattern_words = normalize(words)
+# Create model - 3 layers. First layer 128 neurons, second layer 64 neurons and 3rd output layer contains number of neurons
+# equal to number of intents to predict output intent with softmax
+model = Sequential()
+model.add(Dense(128, input_shape=(len(X_train[0]),), activation='relu'))
+model.add(Dropout(0.6))
+#model.add(Dense(128, activation='relu'))
+model.add(Dense(64, activation='relu'))
+#model.add(Dense(32, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(y_train[0]), activation='softmax'))
 
-#     # count_vect = CountVectorizer(analyzer=normalize)
-#     # words_count = count_vect.fit(words)
-#     # bag =  count_vect.transform(pattern_words)
+# Compile model. Stochastic gradient descent with Nesterov accelerated gradient gives good results for this model
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
-#     # create our bag of words array with 1, if word match found in current pattern
-#     # for w in words:
-#     #     bag.append(1) if w in pattern_words else bag.append(0)
-    
-#     # output is a '0' for each tag and '1' for current tag (for each pattern)
-#     output_row = list(output_empty)
-#     output_row[classes.index(doc[1])] = 1
-    
-#     training.append([bag[doc], output_row])
-# # shuffle our features and turn into np.array
-# #print(training[0])
-# print(bag)
-# print(bag.toarray())
-# print(training[0])
-# print(type(training))
-# print('-----------------------------------------')
-# random.shuffle(training)
-# training = np.array(training)
-# print(training[0])
-# print(type(training))
-# # create train and test lists. X - patterns, Y - intents
-# train_x = list(training[:,0])
-# train_y = list(training[:,1])
-# print("Training data created")
-
-
-# # Create model - 3 layers. First layer 128 neurons, second layer 64 neurons and 3rd output layer contains number of neurons
-# # equal to number of intents to predict output intent with softmax
-# # model = Sequential()
-# # model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
-# # model.add(Dropout(0.6))
-# # model.add(Dense(64, activation='relu'))
-# # model.add(Dropout(0.5))
-# # model.add(Dense(len(train_y[0]), activation='softmax'))
-
-# # # Compile model. Stochastic gradient descent with Nesterov accelerated gradient gives good results for this model
-# # sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-# # model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
-
-# # #fitting and saving the model 
-# # hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
-# # model.save('chatbot_model.h5', hist)
-
-# # print("model created")
+#fitting and saving the model 
+hist = model.fit(X_train, y_train, epochs=200, batch_size=5, verbose=1)
+model.save('chatbot_model.h5', hist)
